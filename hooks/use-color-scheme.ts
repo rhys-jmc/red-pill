161bbd78b9b8ffbaf8ce9a1093796361ba0216d2
Rhys Gevaux <rhys.gevaux@icloud.com>
2021-06-06 15:58:06 +1000

    Update useColorScheme to store last used scheme

--- hooks/use-color-scheme.ts
+++ hooks/use-color-scheme.ts
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import { useColorScheme as _useColorScheme } from "react-native";
 
+import { makeStorage } from "../services/storage";
+
 import type { ColorSchemeName } from "react-native";
+
+const inital = "light" as NonNullable<ColorSchemeName>;
+const storage = makeStorage("color-scheme", { scheme: inital });
 
 // The useColorScheme value is always either light or dark, but the built-in
 // type suggests that it can be null. This will not happen in practice, so this
 // makes it a bit easier to work with.
-export const useColorScheme = (): NonNullable<ColorSchemeName> =>
-  _useColorScheme() as NonNullable<ColorSchemeName>;
+export const useColorScheme = (): NonNullable<ColorSchemeName> => {
+  const [scheme, setScheme] = useState<NonNullable<ColorSchemeName>>(inital);
+  const _scheme = _useColorScheme();
+
+  useEffect(() => {
+    storage
+      .get()
+      .then((x) => setScheme(x.scheme))
+      .catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    if (_scheme) setScheme(_scheme);
+  }, [_scheme]);
+
+  useEffect(() => {
+    storage.set({ scheme }).catch(console.error);
+  }, [scheme]);
+
+  return scheme;
+};
