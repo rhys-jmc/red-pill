cd98712550267c748c5593cdb953a0003af03b32
Rhys Gevaux <rhys.gevaux@icloud.com>
2021-06-24 22:21:44 +1000

    Set JSDoc standards

--- services/tmdb/images.ts
+++ services/tmdb/images.ts
@@ -13,12 +13,30 @@ type ImageInfo = {
   readonly variant: "poster" | "profile" | "backdrop" | "provider";
 };
 
+/**
+ * @param {Object} info Image info variant
+ * @param {string} info.variant TMDb image variant
+ * @returns {string} Local image directory
+ */
 const getImageDir = ({ variant }: Pick<ImageInfo, "variant">): string =>
   `${cacheDirectory}/image/${variant}/`;
 
+/**
+ * @param {Object} info Image info path, variant
+ * @param {number} info.movieId TMDb movie id
+ * @param {string} info.path TMDb image url path
+ * @param {string} info.variant TMDb image variant
+ * @returns {string} Image file URI
+ */
 const imageFileUri = ({ movieId, path, variant }: ImageInfo): string =>
   `${getImageDir({ variant })}${movieId}.${path.split(".").slice(-1)[0]}`;
 
+/**
+ * @param {Object} info Image info path, variant
+ * @param {string} info.path TMDb image url path
+ * @param {string} info.variant TMDb image variant
+ * @returns {string} Image URI
+ */
 const getImageUri = ({
   path,
   variant,
@@ -31,38 +49,56 @@ const getImageUri = ({
       : "w600_and_h900_bestv2"
   }${path}`;
 
-// Checks if image directory exists. If not, creates it
+/**
+ * Checks if image directory exists. If not, creates it
+ *
+ * @param {Object} info Image info variant
+ */
 const ensureDirExists = async (
-  config: Pick<ImageInfo, "variant">
+  info: Pick<ImageInfo, "variant">
 ): Promise<void> => {
-  const dirInfo = await getInfoAsync(getImageDir(config));
+  const dirInfo = await getInfoAsync(getImageDir(info));
   if (!dirInfo.exists) {
-    await makeDirectoryAsync(getImageDir(config), { intermediates: true });
+    await makeDirectoryAsync(getImageDir(info), { intermediates: true });
   }
 };
 
-// Returns URI to our local image file
-// If our image doesn't exist locally, it downloads it
-export const getSingleImageUri = async (config: ImageInfo): Promise<string> => {
-  await ensureDirExists(config);
+/**
+ * Returns URI to our local image file
+ * If our image doesn't exist locally, it downloads it
+ *
+ * @param {Object} info Image info variant, path, movie id
+ * @returns {Promise} image uri string
+ */
+export const getSingleImageUri = async (info: ImageInfo): Promise<string> => {
+  await ensureDirExists(info);
 
-  const fileUri = imageFileUri(config);
+  const fileUri = imageFileUri(info);
   const fileInfo = await getInfoAsync(fileUri);
 
   if (!fileInfo.exists) {
-    await downloadAsync(getImageUri(config), fileUri);
+    await downloadAsync(getImageUri(info), fileUri);
   }
 
   return fileUri;
 };
 
-// Exports shareable URI - it can be shared outside your app
-export const getImageContentUri = async (config: ImageInfo): Promise<string> =>
-  getContentUriAsync(await getSingleImageUri(config));
+/**
+ * Exports shareable URI - it can be shared outside your app
+ *
+ * @param {Object} info Image info variant, path, movie id
+ * @returns {Promise} image content uri string
+ */
+export const getImageContentUri = async (info: ImageInfo): Promise<string> =>
+  getContentUriAsync(await getSingleImageUri(info));
 
-// Deletes whole imagehy directory with all its content
+/**
+ * Deletes whole imagehy directory with all its content
+ *
+ * @param {Object} info Image info variant
+ */
 export const deleteAllImages = async (
-  config: Pick<ImageInfo, "variant">
+  info: Pick<ImageInfo, "variant">
 ): Promise<void> => {
-  await deleteAsync(getImageDir(config));
+  await deleteAsync(getImageDir(info));
 };
